0418b8ddc6b5a3d538f473a1fc67c5d8e077ea86
Moaz Elsherbini <moaaz.elsherbini00@eng-st.cu.edu.eg>
2021-06-13 21:39:35 +0200

    new scheduler

--- scheduler.c
+++ scheduler.c
@@ -2,14 +2,20 @@
 #include "Queue.h"
 
 
+ProcessData prev;
+ProcessData temp;
+//prev.id = 0;
+bool reading = true;
+
+void FCFS(int sem1, int sem2, int algorithm, ProcessData* shmaddr);
 /* arg for semctl system calls. */
 union Semun
 {
     int val;               /* value for SETVAL */
-    struct semid_ds *buf;  /* buffer for IPC_STAT & IPC_SET */
-    ushort *array;         /* array for GETALL & SETALL */
-    struct seminfo *__buf; /* buffer for IPC_INFO */
-    void *__pad;
+    struct semid_ds* buf;  /* buffer for IPC_STAT & IPC_SET */
+    ushort* array;         /* array for GETALL & SETALL */
+    struct seminfo* __buf; /* buffer for IPC_INFO */
+    void* __pad;
 };
 
 void down(int sem)
@@ -42,7 +48,7 @@ void up(int sem)
     }
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     initClk();
 
@@ -54,15 +60,15 @@ int main(int argc, char *argv[])
         perror("Error in create");
         exit(-1);
     }
-    int *shmaddrAlgorithm = shmat(shmidAlgo, (void *)0, 0);
-    if(shmaddrAlgorithm == (void*) -1)	
+    int* shmaddrAlgorithm = shmat(shmidAlgo, (void*)0, 0);
+    if (shmaddrAlgorithm == (void*)-1)
     {
         perror("Error in attaching\n");
         exit(-1);
     }
     //*shmaddrAlgorithm = quantum;
     algorithm = *shmaddrAlgorithm;
-    printf("algo = %d\n",algorithm);
+    printf("algo = %d\n", algorithm);
 
     int shmid;
     shmid = shmget(10, 4096, IPC_CREAT | 0644);
@@ -71,8 +77,8 @@ int main(int argc, char *argv[])
         perror("Error in create");
         exit(-1);
     }
-    ProcessData *shmaddr = shmat(shmid, (void *)0, 0);
-    if(shmaddr == (void*) -1)	
+    ProcessData* shmaddr = shmat(shmid, (void*)0, 0);
+    if (shmaddr == (void*)-1)
     {
         perror("Error in attaching\n");
         exit(-1);
@@ -100,50 +106,78 @@ int main(int argc, char *argv[])
         exit(-1);
     }
     //TODO: implement the scheduler.
-    ProcessData prev;
-    ProcessData temp;
-    bool read = true;
-
-    if(algorithm==1){
-        while(1){
-
-            if(read){
-                down(sem1);
-                prev = temp;
-                temp = *shmaddr;
-                
-                if(temp.id != -1){
-                    if(temp.id!=prev.id){
-                        printf("%d\n",temp.id);
-                        if(algorithm == 1){
-
-                            printf("%d %d %d\n",temp.arrivalTime,temp.runningTime,temp.priority);
-                        }
-
-                    }
-                    
-                }
-                else{
-                    read = false;
-                    printf("stop reading\n");
-                }
-                up(sem2);
-
-            }
-            else{
-                //printf("done reading\n");
-            }
-            //printf("read = %d\n",read);
 
 
-
-            //exit(0);
-        }
+    if (algorithm == 1) {
+        FCFS(sem1, sem2, algorithm, shmaddr);
     }
-    
-    
+
+
 
 
     //TODO: upon termination release the clock resources.
     destroyClk(true);
+}
+
+void FCFS(int sem1, int sem2, int algorithm, ProcessData* shmaddr) {
+    Queue* readyQueue = (Queue*)malloc(sizeof(Queue*));
+    char buf[50];
+    getcwd(buf, sizeof(buf));
+    ProcessData runningProcess;
+    runningProcess.remainingTime = 0;
+    int prevTime;
+    int currentTime;
+
+    while (1) {
+
+        if (reading) {
+
+            down(sem1);
+
+
+            temp = *shmaddr;
+            //printf("%d\n",temp.id);
+            if (temp.id != -1) {
+                if (temp.id != prev.id && temp.arrivalTime > 0) {
+                    //printf("%d\n",temp.id);
+                    printf("%d %d %d\n", temp.arrivalTime, temp.runningTime, temp.priority);
+                    enqueue(readyQueue, temp);
+                    int processPid = fork();
+                    temp.pid = processPid;
+                    if (processPid == 0) {
+                        //temp.pid = getpid();
+                        char buffer[265];
+                        //itoa(temp.runningTime,buffer,10); 
+                        strcat(buf, "/process.out");
+                        execl(buf, "process.out", temp.runningTime, NULL);
+                    }
+                }
+
+            }
+            else {
+                reading = false;
+                printf("stop reading\n");
+            }
+            up(sem2);
+            prev = temp;
+        }
+        ProcessData firstInQueue;
+        if (runningProcess.remainingTime == 0 && peek(readyQueue, &firstInQueue)) {
+            dequeue(readyQueue, &runningProcess);
+        }
+        // if(runningProcess.runningTime != 0){
+        //     prevTime = currentTime;
+        //     currentTime = getClk();
+        //     if(prevTime!=currentTime){
+
+        //         //kill(runningProcess.pid,SIGUSR1);
+        //         printf("Current Time is %d\n", currentTime);
+        //     }
+        // }
+
+        // if(reading == false && !peek(readyQueue,&firstInQueue) && runningProcess.remainingTime == 0){
+        //     printf("done scheduling");
+        // }
+
+    }
 }
